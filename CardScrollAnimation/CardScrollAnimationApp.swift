51dd57d409db7f039e53d0f192ea86b5b58419a3
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-06-08 11:20:21 +0200

    Initial Commit

---
+++ CardScrollAnimation/CardScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardScrollAnimationApp.swift
+//  CardScrollAnimation
+//
+//  Created by Alex De Pasquale on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CardScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
